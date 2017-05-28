d13b9aaf3ae9c69af02c7d1a6d7304bb61b5bcb9
Mert Bulent Sariyildiz <mbsariyildiz@gmail.com>
2017-05-28 11:21:16 +0300

    file comment

--- score_ale.py
+++ score_ale.py
@@ -1,9 +1,20 @@
+"""
+Mert Bulent Sariyildiz,
+mert.sariyildiz@bilkent.edu.tr
+28 May, 2017
+
+This work is a part of Python implementation of the MATLAB code published by Yongqin Xian.
+For source, please see
+https://www.mpi-inf.mpg.de/departments/computer-vision-and-multimodal-computing/research/zero-shot-learning/zero-shot-learning-the-good-the-bad-and-the-ugly/
+"""
+
+
 import numpy as np
 import h5py
 
 def score_ale(X, S_gt, model_path):
   """
-  
+  Score function of ALE published by Yongqin Xian.
   """
   with h5py.File(model_path, 'r') as hf:
     W = hf['W'].value
